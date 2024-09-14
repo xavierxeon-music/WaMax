ecb794c1014b43c0a023d8c5eda2712c7e082946
Ralf Waspe <rwaspe@icloud.com>
2024-09-14 19:41:35 +0200

    params cart 2 sphere

--- source/maxobjects/wa.cart2sphere/wa.cart2sphere.cpp
+++ source/maxobjects/wa.cart2sphere/wa.cart2sphere.cpp
@@ -2,15 +2,24 @@
 
 #include <patcher.h>
 
-Cart2Sphere::Cart2Sphere()
+Cart2Sphere::Cart2Sphere(const atoms& args)
    : object<Cart2Sphere>()
    , cartesian()
-   , input{this, "(list) 7 bit list"}
+   , input{this, "set values"}
    , output{this, "(int) integer value"}
-   , xMessage{this, "x", "x value.", Patcher::minBind(this, &Cart2Sphere::xFunction)}
-   , yMessage{this, "y", "y value.", Patcher::minBind(this, &Cart2Sphere::yFunction)}
-   , zMessage{this, "z", "z value.", Patcher::minBind(this, &Cart2Sphere::zFunction)}
+   , xValue{this, "x", "x value", Patcher::minBind(this, &Cart2Sphere::xFunction)}
+   , yValue{this, "y", "y value", Patcher::minBind(this, &Cart2Sphere::xFunction)}
+   , zValue{this, "z", "z value", Patcher::minBind(this, &Cart2Sphere::xFunction)}
+   , doubleClick{this, "dblclick", Patcher::minBind(this, &Cart2Sphere::doubleClickedFunction)}
 {
+   if (args.size() > 0)
+      cartesian.setA(args[0]);
+   if (args.size() > 1)
+      cartesian.setB(args[1]);
+   if (args.size() > 2)
+      cartesian.setC(args[2]);
+
+   calcluate();
 }
 
 atoms Cart2Sphere::xFunction(const atoms& args, const int inlet)
@@ -41,6 +50,13 @@ atoms Cart2Sphere::zFunction(const atoms& args, const int inlet)
       cartesian.setC(args[0]);
       calcluate();
    }
+   return {};
+}
+
+atoms Cart2Sphere::doubleClickedFunction(const atoms& args, const int inlet)
+{
+   calcluate();
+
    return {};
 }
 
--- source/maxobjects/wa.cart2sphere/wa.cart2sphere.h
+++ source/maxobjects/wa.cart2sphere/wa.cart2sphere.h
@@ -13,7 +13,7 @@ public:
    MIN_DESCRIPTION{"convert 3d carteasion coordinates to spherical"};
 
 public:
-   Cart2Sphere();
+   Cart2Sphere(const atoms& args = {});
 
 private:
    Linalg::Vector3 cartesian;
@@ -22,14 +22,18 @@ public:
    inlet<> input;
    outlet<> output;
 
-   message<> xMessage;
-   message<> yMessage;
-   message<> zMessage;
+   message<> xValue;
+   message<> yValue;
+   message<> zValue;
+
+   message<> doubleClick;
 
 private:
    atoms xFunction(const atoms& args, const int inlet);
    atoms yFunction(const atoms& args, const int inlet);
    atoms zFunction(const atoms& args, const int inlet);
+
+   atoms doubleClickedFunction(const atoms& args, const int inlet);
 
    void calcluate();
 };
