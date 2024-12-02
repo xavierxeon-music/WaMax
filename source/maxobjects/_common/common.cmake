ef7b4e488b58d0faaf3fed50fd82978e3be91088
Ralf Waspe <rwaspe@icloud.com>
2024-12-02 07:57:36 +0100

    moved stuff to common cmake

--- source/maxobjects/_common/common.cmake
+++ source/maxobjects/_common/common.cmake
@@ -1,1 +1,6 @@
 include_directories(${CMAKE_CURRENT_LIST_DIR})
+
+if(APPLE)
+   set(CMAKE_OSX_DEPLOYMENT_TARGET "15.0") # for Qt
+   target_compile_features(${PROJECT_NAME} PRIVATE cxx_std_20)
+endif()
