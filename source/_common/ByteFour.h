317914d7e23058051ec993da28973abcf682df59
Ralf Waspe <rwaspe@icloud.com>
2026-03-06 08:21:00 +0100

    cleanup

---
+++ source/_common/ByteFour.h
@@ -0,0 +1,14 @@
+#ifndef ByteFourH
+#define ByteFourH
+
+class ByteFour
+{
+public:
+   ByteFour();
+};
+
+#ifndef ByteFourHPP
+#include "ByteFour.hpp"
+#endif // NOT ByteFourHPP
+
+#endif // NOT ByteFourH
---
+++ source/_common/ByteFour.hpp
@@ -0,0 +1,11 @@
+#ifndef ByteFourHPP
+#define ByteFourHPP
+
+#include "ByteFour.h"
+
+inline ByteFour::ByteFour()
+{
+}
+
+#endif // NOT ByteFourHPP
+
